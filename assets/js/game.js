3f148d7813be2995c5c5a5d18b660210389153d7
aidyel <aidayeli@icloud.com>
2022-02-06 21:21:59 -0500

    javaScript updated

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,9 @@
 var playerName = window.prompt("what is your robot's name?")
 var playerHealth = 100;
+
+if (playerHealth > 0) {
+    console.log("Your player is still alive!");
+}
 var playerAttack = 10;
 
 var enemyName = "Roborto";
@@ -12,19 +16,30 @@ var fight = function() {
 
 
   //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-    enemyHelth = enemyHealth - playerAttack;
+    enemyHealth = enemyHealth - playerAttack;
 
-  // Log a resulting message to the console so we know that it worked.
     console.log(
         playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining. "
     );
+    if (enemyHealth <=0) {
+        window.alert(enemyName + " has died! ")
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left. ")
+    }
 
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
      playerHealth = playerHealth - enemyAttack;
 
   // Log a resulting message to the console so we know that it worked.
-    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-
+  console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.")
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died! ")
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left. ")
+        
+    }
 }
 
 fight();
